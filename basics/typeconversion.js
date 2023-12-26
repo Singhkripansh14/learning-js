34e4d830a322b553ae3ee60dd10005738627df43
Kripansh Singh <139781223+Singhkripansh14@users.noreply.github.com>
2023-12-26 11:27:28 +0000

    conversion of datatypes

---
+++ basics/typeconversion.js
@@ -0,0 +1,14 @@
+let name="kripansh"
+let id=131;
+let idString='131abc'
+// console.log(typeof idString)
+let idInNumber=Number(idString)
+let score="kripu"
+let scoreInNumber=Number(score)
+// console.log(typeof idInNumber)
+// console.log(idInNumber)
+// console.log(typeof NaN)
+console.log(typeof scoreInNumber)
+console.log(scoreInNumber)
+
+let islogedIn=1
