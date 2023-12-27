c3d08cba8d74c44376ba05b7f4f9e600d6408c46
Kripansh Singh <139781223+Singhkripansh14@users.noreply.github.com>
2023-12-27 13:07:44 +0000

    update in typeconversion

--- basics/typeconversion.js
+++ basics/typeconversion.js
@@ -1,25 +1,46 @@
-//string ->number
+//*******string ->number******
 let myName='kripansh'
 let roll_no='131'
 let password='2001abc'
-
-let convertedAsNumber=Number(myName)
-console.log(converetedAsNumber)
+let sum;
+let convertedAsNumber=Number(roll_no)
+console.log(convertedAsNumber)
 console.log(typeof convertedAsNumber)
 
-//Number->boolean
+// string with only digits=>number with that digit
+// string with digits & alphabets=>NaN
+// string with only alphabets=>NaN
+// undefined value =>NaN
+
+//*******Number->boolean*********
 const pi=3.14
 let id=0
 let result=null
 let finding=NaN
 
-let convertedAsBoolean=Boolean(pi)
+let convertedAsBoolean1=Boolean(id)
 
-console.log(convertedAsBoolean)
-console.log(typeof covertedAsBoolean)
+console.log(convertedAsBoolean1)
+console.log(typeof convertedAsBoolean1)
 
-//string->boolean
+//any number except 0 =>true
+//0=>false
+// null=>false
+// NaN=>false
+
+//*****string->boolean*********
 
 let MyClass='IT'
+let data=''
+let date=undefined
+let mixed="krip2001"
+let convertedAsBoolean2=Boolean(MyClass)
 
 
+console.log(convertedAsBoolean2)
+console.log(typeof convertedAsBoolean2)
+
+// empty string =>false
+// string of alphabets =>true
+// string of alphabets and digits => true
+// undefined => false
