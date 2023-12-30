ff675aa372fafa1e017839f80b693ea7faa1e4ff
Kripansh Singh <139781223+Singhkripansh14@users.noreply.github.com>
2023-12-30 18:12:01 +0000

    new string file

---
+++ basics/string.js
@@ -0,0 +1,10 @@
+const name='    kripansh    '
+let city=new String('I live in delhi')
+
+// console.log(name.substring(0,3))
+// console.log(name.slice(-4,2))
+//console.log(name.substring(0,3))
+// console.log(city.split(' '))
+// const url='https\\kripansh%20.com'
+// console.log(url.replace("%20","$"))
+console.log(name.trim())
